d4dff0d08ad18e2a40339741b421efdaf5bba074
Paul van Woensel <pvanwoensel@gmail.com>
2023-02-19 12:44:13 -0600

    Initial Commit

---
+++ MoodMix/MoodMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodMixApp.swift
+//  MoodMix
+//
+//  Created by Paul van Woensel on 2/19/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
